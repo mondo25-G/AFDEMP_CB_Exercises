6181e6fd6e967a9652a8a12d808412dbbeb08b60
mondo25-G <73531724+mondo25-G@users.noreply.github.com>
2020-10-29 00:14:52 +0200

    Add files via upload
    Uploads:
    - Project that converts a binary number to decimal
    - Project that prints a double arrow on console

---
+++ GC_Exercises_28_10_2020/ConvertBinaryToDecimal/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GX_Exercises_28_10_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input a binary number: "); // of arbitrary length not just 4
+
+            string binaryString = Console.ReadLine();
+
+            isBinary(binaryString);
+
+
+
+        }
+        static void isBinary(string binaryString)
+        {
+            int isBinaryString = 0;
+            foreach (char bitChar in binaryString)
+            {
+                if (bitChar == '1' || bitChar == '0')
+                    isBinaryString++;
+                else continue;
+            }
+
+            if (isBinaryString == binaryString.Length)
+            {
+                ConvertBinaryStringToDecimal(binaryString);
+            }
+            else
+            {
+                Console.WriteLine("You did not input a binary number");
+            }
+        }
+
+        static void ConvertBinaryStringToDecimal(string binaryString)
+        {
+            double sum = 0;
+            int k = binaryString.Length - 1;
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+
+                sum += ConvertBitAtGivenIndexFromBinaryToDecimal(ConvertBitCharToInt(binaryString[i]), k);
+                k--;
+
+            }
+            Console.WriteLine($"{binaryString} corresponds to {sum}");
+        }
+
+        static double ConvertBitAtGivenIndexFromBinaryToDecimal(int bitAtEachIndex, int index)
+        {
+            double bitAtEachIndexToDecimal = bitAtEachIndex * Math.Pow(2, index);
+            return bitAtEachIndexToDecimal;
+        }
+
+
+        static int ConvertBitCharToInt(char bitChar)
+        {
+            int bitAtEachIndex = int.Parse(bitChar.ToString());
+            return bitAtEachIndex;
+        }
+
+
+    }
+
+
+}
---
+++ GC_Exercises_28_10_2020/StarsAndSpacesArrowNew/Program.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarsSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreateDoubleArrow();
+
+
+            /*    ^         ^
+             *   ^^         ^^
+             *  ^^^^^^^^^^^^^^^  
+             * ^^^^^^^^^^^^^^^^^
+             *  ^^^^^^^^^^^^^^^
+             *   ^^         ^^ 
+             * ?  ^         ^
+             */
+
+            //needs 1) max no of spaces before triangle base : 4
+            //      2) length of triangle base : 2
+            //      3) spaces between trianles : 9
+            //      4) symbol to create double arrow: ^
+
+        }
+
+
+
+   
+        static void CreateDoubleArrow()
+        {
+            Console.Write("Enter maximum base length of triangle: ");
+            int triangleBase = int.Parse(Console.ReadLine());
+            Console.Write("Enter maximum length of spaces before/after triangle base: ");
+            int spaceBeforeBase = int.Parse(Console.ReadLine());
+            Console.Write("Enter maximum length of spaces between triangles: ");
+            int spaceBetweenTriangles = int.Parse(Console.ReadLine());
+            Console.Write("Enter symbol to draw double arrow: ");
+            char symbol = char.Parse(Console.ReadLine());
+            DoubleArrow(spaceBeforeBase, triangleBase, spaceBetweenTriangles, symbol);
+
+
+
+        }
+
+
+
+        static string XSymbol(int noOfSymbols, char symbol)
+        {
+            string symbolString = "";
+            for (int i = 0; i < noOfSymbols; i++)
+            {
+                symbolString += symbol;
+            }
+            return (symbolString);
+        }
+
+
+        static string XSpaces(int noOfSpaces)
+        {
+            string spaces = "";
+            for (int i = 0; i < noOfSpaces; i++)
+            {
+                spaces += " ";
+            }
+            return (spaces);
+        }
+
+        static void DoubleArrow(int spaceBeforeBase, int triangleBase, int spaceBetweenTriangles, char symbol)
+        {
+            
+            TopPart(spaceBeforeBase, triangleBase, spaceBetweenTriangles,symbol);
+            BottomPart(spaceBeforeBase, triangleBase, spaceBetweenTriangles, symbol);
+            
+
+        }
+
+        static void TopPart(int spaceBeforeBase, int triangleBase, int spaceBetweenTriangles, char symbol)
+        {
+            int arrowHeight = spaceBeforeBase + triangleBase;
+            for (int i = 0; i <= arrowHeight; i++)
+            {
+                if (i <= triangleBase)
+                {
+                    Console.WriteLine(XSpaces(arrowHeight - i) + XSymbol(i, symbol) + XSpaces(spaceBetweenTriangles) + XSymbol(i, symbol) + XSpaces(arrowHeight - i));
+                }
+                else
+                {
+                    Console.WriteLine(XSpaces(arrowHeight - i) + XSymbol(i, symbol) + XSymbol(spaceBetweenTriangles, symbol) + XSymbol(i, symbol) + XSpaces(arrowHeight - i));
+                }
+
+            }
+        }
+
+        static void BottomPart(int spaceBeforeBase, int triangleBase, int spaceBetweenTriangles, char symbol)
+        {
+
+            int arrowHeight = spaceBeforeBase + triangleBase;
+            for (int i = 0; i<=arrowHeight-1; i++)
+            {
+                if (i <spaceBeforeBase)
+                {
+                    Console.WriteLine(XSpaces(i) + XSymbol(arrowHeight - i, symbol) + XSymbol(spaceBetweenTriangles, symbol) + XSymbol(arrowHeight - i, symbol));
+
+                }
+                else
+                {
+                    Console.WriteLine(XSpaces(i) + XSymbol(arrowHeight - i, symbol) + XSpaces(spaceBetweenTriangles) + XSymbol(arrowHeight - i, symbol));
+                }
+
+
+
+
+
+            }
+        }
+
+        }
+}
