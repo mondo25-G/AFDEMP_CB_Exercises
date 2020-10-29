5fa10e9a3a9b1bbfc1c505adef70d7b28e38978e
mondo25-G <73531724+mondo25-G@users.noreply.github.com>
2020-10-30 00:50:16 +0200

    Add files via upload
    -Added a simple first exercise on Classes

---
+++ GC_Exercises_30_10_2020/SimpleIntegerCalculator/IntegerCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIntegerCalculator
+{
+    class IntegerCalculator
+    {
+        int a; 
+        int b;
+
+        public IntegerCalculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Add()
+        {
+            int sum = a + b;
+            Console.WriteLine($"( {a} ) + ( {b} ) = {sum}");
+        }
+
+        public void Subtract()
+        {
+            int difference = a - b;
+            Console.WriteLine($"( {a} ) - ( {b} ) = {difference}");
+        }
+        public void Multiply()
+        {
+            int product = a * b;
+            Console.WriteLine($"( {a} ) x ( {b} ) = {product}");
+        }
+
+        public void Divide()
+        {
+            if (b != 0)
+            {
+                int quotient = a / b;
+                int remainder = a % b;
+                string message = (remainder == 0) ? $"( {a} ) / ( {b} ) = {quotient}" : $"{a} / {b} = {quotient} with remainder = {remainder}";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine($"( {a} ) / ( {b} ) => Error.");
+            }
+        }
+
+        public void PrintAllCalculations()
+        {
+            Add();
+            Subtract();
+            Multiply();
+            Divide();
+        }
+    }
+}
---
+++ GC_Exercises_30_10_2020/SimpleIntegerCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIntegerCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerCalculator calc1 = new IntegerCalculator(0, 0);
+            IntegerCalculator calc2 = new IntegerCalculator(13, -4);
+            IntegerCalculator calc3 = new IntegerCalculator(-2, -5);
+            IntegerCalculator calc4 = new IntegerCalculator(80, 4);
+
+            
+
+            calc1.PrintAllCalculations();
+            calc2.PrintAllCalculations();
+            calc3.PrintAllCalculations();
+            calc4.PrintAllCalculations();
+        }
+    }
+}
